494e3df8d2776870d5aba05dfd34e8ef506ba5f2
Vinicius Bortolotti Lemos <viniciusblemos@hotmail.com>
2020-10-23 15:18:21 -0300

    1013.py

--- 1013.py
+++ 1013.py
@@ -2,3 +2,4 @@ a, b, c = input().split(" ")
 MaiorAB = ((int(a)+int(b)+(abs(int(a)-int(b))))/2)
 MaiorABC = ((int(MaiorAB)+int(c)+(abs(int(MaiorAB)-int(c))))/2)
 print(int(MaiorABC), "eh o maior")
+#bla
