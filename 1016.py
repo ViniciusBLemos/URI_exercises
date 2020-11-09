b1b9c4e922f4a6c684e0fd1d0f30c1418df75b9b
Vinicius Bortolotti Lemos <viniciusblemos@hotmail.com>
2020-11-09 18:47:04 -0300

    1114.py

---
+++ 1016.py
@@ -0,0 +1,3 @@
+x = input()
+TEMPO = (int(x)*2)
+print(TEMPO, "minutos")
---
+++ 1114.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+SENHA = input()
+
+while int(SENHA) != 2002:
+  print("Senha Invalida")
+  SENHA = int(input())
+
+print("Acesso Permitido") 
